88a83cdb40adda3c240e639290782d40075297fd
mayak <webmaster@notmail.yes>
2023-10-04 16:53:39 +0400

    1

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use actix_web::{App, get, HttpRequest, HttpResponse, HttpServer, web};
 use actix_web::http::StatusCode;
-use env_logger::Env;
-
+use env_logger::{Env};
+use actix_web::middleware::Logger;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .service(check)
     }
-    ).bind("127.0.0.1:8080")?
+    ).bind("0.0.0.0:8080")?
         .workers(1)
         .run()
         .await
